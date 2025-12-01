07df259c885e31d8c4f27f69c33d05f4e1f730ed
garystarr-surgi <gary.starr@surgishop.com>
2025-12-01 16:24:10 -0500

    Update surgi_general_ledger.js

--- general_ledger_button/public/js/surgi_general_ledger.js
+++ general_ledger_button/public/js/surgi_general_ledger.js
@@ -1,6 +1,33 @@
 frappe.query_reports["Surgi General Ledger"] = frappe.query_reports["Surgi General Ledger"] || {};
 
+// Override refresh to prevent running without required filters
+var original_refresh = frappe.query_reports["Surgi General Ledger"].refresh;
+
+frappe.query_reports["Surgi General Ledger"].refresh = function(filters) {
+    // Check if required filters are present
+    if (!filters || !filters.from_date || !filters.to_date) {
+        // Don't run the query if required filters are missing
+        // The report will wait for user to set filters
+        console.log("Waiting for filters to be set...");
+        return;
+    }
+    
+    // Call original refresh if it exists and filters are present
+    if (typeof original_refresh === 'function') {
+        return original_refresh.call(this, filters);
+    }
+};
+
+// Add print button
+var original_onload = frappe.query_reports["Surgi General Ledger"].onload;
+
 frappe.query_reports["Surgi General Ledger"].onload = function(report) {
+    // Call original onload if it exists
+    if (typeof original_onload === 'function') {
+        original_onload.call(this, report);
+    }
+    
+    // Add print button after report is loaded
     setTimeout(function() {
         if (report && report.page && report.page.add_inner_button) {
             report.page.add_inner_button(__("Print Statement"), function() {
@@ -22,5 +49,5 @@ frappe.query_reports["Surgi General Ledger"].onload = function(report) {
                 window.open(print_url, '_blank');
             });
         }
-    }, 1000);
+    }, 1500);
 };
