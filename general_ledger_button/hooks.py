d8d996053c3535cd525d0060f1343c2436ac75ff
garystarr-surgi <gary.starr@surgishop.com>
2025-12-01 15:04:21 -0500

    Update hooks.py

--- general_ledger_button/hooks.py
+++ general_ledger_button/hooks.py
@@ -1,4 +1,12 @@
-
+app_version = "0.0.1"
+app_name = "general_ledger_button"
+app_title = "General Ledger Button"
+app_publisher = "SurgiShop"
+app_description = "Add a print button to general ledger"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
 
 app_include_js = [
     "your_app_name/public/js/surgi_general_ledger.js"
