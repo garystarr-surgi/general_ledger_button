fc0489f3423f489a3750014138a3178b6c541944
garystarr-surgi <gary.starr@surgishop.com>
2025-12-08 15:22:39 -0500

    Implement test data in get_data function
    Added hardcoded test data to the get_data function for initial testing and verification.

--- general_ledger_button/report/surgi_general_ledger/surgi_general_ledger.py
+++ general_ledger_button/report/surgi_general_ledger/surgi_general_ledger.py
@@ -60,31 +60,55 @@ def get_data(filters):
     """
     Fetch report data
     """
+    # TEST: Return hardcoded data first to verify the function is being called
+    test_data = [
+        {
+            "posting_date": "2024-01-01",
+            "description": "Test Entry 1",
+            "debit": 1000.00,
+            "credit": 0.00,
+            "amount": 1000.00
+        },
+        {
+            "posting_date": "2024-01-02",
+            "description": "Test Entry 2",
+            "debit": 0.00,
+            "credit": 500.00,
+            "amount": -500.00
+        }
+    ]
+    
+    return test_data
+    
+    # Original query code (commented out for testing)
+    """
     params = {
         'from_date': filters.get('from_date'),
         'to_date': filters.get('to_date')
     }
     
-    query = """
+    query = '''
         SELECT
-            gle.posting_date AS posting_date,
+            gle.posting_date,
             CONCAT(gle.voucher_type, ' ', gle.voucher_no) AS description,
-            gle.debit AS debit,
-            gle.credit AS credit,
+            gle.debit,
+            gle.credit,
             (gle.debit - gle.credit) AS amount
         FROM `tabGL Entry` gle
         WHERE
             gle.is_cancelled = 0
             AND gle.party_type = 'Customer'
-            AND gle.posting_date BETWEEN %(from_date)s AND %(to_date)s
-    """
+            AND gle.posting_date >= %(from_date)s
+            AND gle.posting_date <= %(to_date)s
+    '''
     
     if filters.get('customer'):
         query += " AND gle.party = %(customer)s"
         params['customer'] = filters.get('customer')
     
-    query += " ORDER BY gle.posting_date ASC, gle.creation ASC"
+    query += " ORDER BY gle.posting_date ASC"
     
     data = frappe.db.sql(query, params, as_dict=1)
     
     return data
+    """
