e5a3551f159a9eee7ea89933326cda87c2d482bd
garystarr-surgi <gary.starr@surgishop.com>
2025-12-08 14:53:08 -0500

    Refactor Surgi General Ledger report functions

--- general_ledger_button/report/surgi_general_ledger/surgi_general_ledger.py
+++ general_ledger_button/report/surgi_general_ledger/surgi_general_ledger.py
@@ -1,70 +1,106 @@
+# surgi_general_ledger.py
 import frappe
+from frappe import _
 
 def execute(filters=None):
     """
     Execute function for Surgi General Ledger Report
-    This ensures filters are properly handled
     """
     # Ensure filters is a dictionary
     if not filters:
         filters = {}
     
-    # Get filter values - these come from the report filters UI
+    # Get filter values
     from_date = filters.get('from_date')
     to_date = filters.get('to_date')
     customer = filters.get('customer')
     
     # Validate required filters
     if not from_date or not to_date:
-        frappe.throw("Please select From Date and To Date")
+        frappe.throw(_("Please select From Date and To Date"))
     
-    # Build WHERE conditions
-    conditions = [
-        "gle.is_cancelled = 0",
-        "gle.party_type = 'Customer'",
-        "gle.posting_date >= %(from_date)s",
-        "gle.posting_date <= %(to_date)s"
+    # Get columns and data
+    columns = get_columns()
+    data = get_data(filters)
+    
+    return columns, data
+
+def get_columns():
+    """
+    Define columns in dictionary format for Script Report
+    """
+    return [
+        {
+            "fieldname": "posting_date",
+            "label": _("Date"),
+            "fieldtype": "Date",
+            "width": 100
+        },
+        {
+            "fieldname": "description",
+            "label": _("Description"),
+            "fieldtype": "Data",
+            "width": 300
+        },
+        {
+            "fieldname": "debit",
+            "label": _("Debit"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "credit",
+            "label": _("Credit"),
+            "fieldtype": "Currency",
+            "width": 120
+        },
+        {
+            "fieldname": "amount",
+            "label": _("Balance"),
+            "fieldtype": "Currency",
+            "width": 120
+        }
     ]
+
+def get_data(filters):
+    """
+    Fetch report data
+    """
+    # Build WHERE conditions
+    conditions = []
+    params = {
+        'from_date': filters.get('from_date'),
+        'to_date': filters.get('to_date')
+    }
     
     # Add customer filter if specified
-    if customer:
+    if filters.get('customer'):
         conditions.append("gle.party = %(customer)s")
+        params['customer'] = filters.get('customer')
     
-    where_clause = " AND ".join(conditions)
+    # Build additional WHERE clause
+    additional_conditions = ""
+    if conditions:
+        additional_conditions = "AND " + " AND ".join(conditions)
     
     # Build the query
-    query = """
+    query = f"""
         SELECT
             gle.posting_date AS posting_date,
-            CONCAT(gle.voucher_type, " ", gle.voucher_no) AS description,
+            CONCAT(gle.voucher_type, ' ', gle.voucher_no) AS description,
             gle.debit AS debit,
             gle.credit AS credit,
             (gle.debit - gle.credit) AS amount
         FROM `tabGL Entry` gle
-        WHERE {where_clause}
+        WHERE
+            gle.is_cancelled = 0
+            AND gle.party_type = 'Customer'
+            AND gle.posting_date BETWEEN %(from_date)s AND %(to_date)s
+            {additional_conditions}
         ORDER BY gle.posting_date ASC, gle.creation ASC
-    """.format(where_clause=where_clause)
+    """
     
-    # Prepare parameters for SQL query
-    params = {
-        'from_date': from_date,
-        'to_date': to_date
-    }
+    # Execute query and return as list of dictionaries
+    data = frappe.db.sql(query, params, as_dict=1)
     
-    if customer:
-        params['customer'] = customer
-    
-    # Define columns for the report
-    columns = [
-        "Posting Date:Date:120",
-        "Description:Data:200",
-        "Debit:Currency:120",
-        "Credit:Currency:120",
-        "Amount:Currency:120"
-    ]
-    
-    # Execute query
-    data = frappe.db.sql(query, params, as_dict=0)
-    
-    return columns, data
-
+    return data
