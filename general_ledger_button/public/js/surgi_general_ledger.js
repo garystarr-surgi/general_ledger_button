545591924c60988994598ba5221f6d258f16aed7
garystarr-surgi <gary.starr@surgishop.com>
2025-12-09 08:33:17 -0500

    Update surgi_general_ledger.js

--- general_ledger_button/public/js/surgi_general_ledger.js
+++ general_ledger_button/public/js/surgi_general_ledger.js
@@ -1,27 +1,4 @@
 frappe.query_reports["Surgi General Ledger"] = {
-	"filters": [
-		{
-			"fieldname": "from_date",
-			"label": __("From Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.add_months(frappe.datetime.nowdate(), -1),
-			"reqd": 1
-		},
-		{
-			"fieldname": "to_date",
-			"label": __("To Date"),
-			"fieldtype": "Date",
-			"default": frappe.datetime.nowdate(),
-			"reqd": 1
-		},
-		{
-			"fieldname": "customer",
-			"label": __("Customer"),
-			"fieldtype": "Link",
-			"options": "Customer"
-		}
-	],
-	
 	"onload": function(report) {
 		// Add print button after report is loaded
 		setTimeout(function() {
