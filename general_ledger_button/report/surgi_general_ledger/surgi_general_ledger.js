b80c8ba45db013d129fcda24116c20ada698a306
garystarr-surgi <gary.starr@surgishop.com>
2025-12-09 08:58:22 -0500

    Add filters to Surgi General Ledger report

---
+++ general_ledger_button/report/surgi_general_ledger/surgi_general_ledger.js
@@ -0,0 +1,23 @@
+frappe.query_reports["Surgi General Ledger"] = {
+	"filters": [
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.nowdate(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.nowdate(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		}
+	]
