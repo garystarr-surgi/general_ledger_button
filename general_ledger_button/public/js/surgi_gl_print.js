ca4d66b2349a11bf682d3ebbe406c3ec0619fa70
garystarr-surgi <gary.starr@surgishop.com>
2025-12-09 11:27:33 -0500

    Refactor print button to use custom print method

--- general_ledger_button/public/js/surgi_gl_print.js
+++ general_ledger_button/public/js/surgi_gl_print.js
@@ -23,45 +23,13 @@
         if (frappe.query_report.page && frappe.query_report.page.add_inner_button) {
             frappe.query_report.page.add_inner_button(__("Print Statement"), function() {
                 let filters = frappe.query_report.get_filter_values() || {};
-                let report_name = 'Surgi General Ledger';
-                let print_format = 'Surgi Customer Statement';
                 
-                // For Query Reports with custom print formats, we need to use a server method
-                // or pass the data differently. Try using Frappe's print dialog approach.
-                // First, let's try using the report's print method if available
-                if (frappe.query_report.print_format) {
-                    // Use the report's built-in print method
-                    frappe.query_report.print_format = print_format;
-                }
-                
-                // Build the print URL - for Query Reports, we need to execute the query first
-                // Try using the standard printview but with proper parameters
+                // Call our custom print method
+                let url = '/api/method/general_ledger_button.print_utils.print_surgi_general_ledger';
                 let params = new URLSearchParams();
+                params.append('filters', JSON.stringify(filters));
                 
-                // For Query Reports, we might need to use a different approach
-                // Try passing report_name as a special parameter
-                params.append('doctype', 'Report');
-                params.append('name', report_name);
-                params.append('print_format', print_format);
-                params.append('report_name', report_name); // Also include as separate param
-                
-                // Add filter parameters - encode them properly
-                Object.keys(filters).forEach(function(key) {
-                    if (filters[key] !== null && filters[key] !== undefined && filters[key] !== '') {
-                        // Format dates properly if needed
-                        let value = filters[key];
-                        if (value instanceof Date) {
-                            value = frappe.datetime.obj_to_str(value);
-                        }
-                        params.append('filters[' + key + ']', value);
-                    }
-                });
-                
-                let print_url = '/printview?' + params.toString();
-                
-                console.log("Print URL:", print_url);
-                console.log("Filters:", filters);
-                window.open(print_url, '_blank');
+                window.open(url + '?' + params.toString(), '_blank');
             });
             
             frappe.query_report._print_button_added = true;
